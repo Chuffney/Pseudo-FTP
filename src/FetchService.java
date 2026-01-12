1d8e154b5ed51976775bf13ff0d54f428a266305
CasualJavaUser <max.berezowski@gmail.com>
2026-01-12 23:14:54 +0100

    added a progress bar when fetching; moved "fetch" method to FetchService; removed EnumUtil

---
+++ src/FetchService.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+public class FetchService {
+    private static final int DEFAULT_BUFFER_SIZE = 8192;
+    private static final int STEP_SIZE = 50;
+
+    public static void fetch() throws IOException {
+        try (Socket clientSocket = ConnectionService.openSocket();
+             InputStream in = clientSocket.getInputStream();
+             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+        ) {
+            String filePath = ArgumentParsing.getParamValue(Param.FILE_PATH);
+            out.write(Command.FETCH.code);
+            out.write(filePath);
+            out.newLine();
+            out.flush();
+
+            System.out.println("sent request");
+
+            byte code = (byte) in.read();
+
+            if (code == ResponseCode.NOT_FOUND.code) {
+                System.out.println("File not found!");
+            } else if (code == ResponseCode.FORBIDDEN.code) {
+                System.out.println("Access denied!");
+            } else if (code == ResponseCode.OK.code) {
+                byte[] byteArray = in.readNBytes(Long.BYTES);
+                long fileSize = bytesToLong(byteArray);
+
+                long fetchStart = System.currentTimeMillis();
+
+                File file = new File(new File(filePath).getName());
+                System.out.println("Transferring file to " + file.getName());
+                FileOutputStream fos = new FileOutputStream(file);
+                transfer(in, fos, fileSize);
+
+                long time = System.currentTimeMillis() - fetchStart;
+                System.out.println("time: " + (float)time/1000f + " s");
+                System.out.println("size: " + fileSize/1000 + " kB");
+                System.out.println("speed: " + (fileSize / time) + " kB / s");
+
+                fos.close();
+            }
+        }
+    }
+
+    public static long bytesToLong(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.put(bytes);
+        buffer.flip();
+        return buffer.getLong();
+    }
+
+    public static void transfer(InputStream in, OutputStream out, long size) throws IOException {
+        Objects.requireNonNull(out, "out");
+        long step = size/STEP_SIZE;
+        long transferred = 0;
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+
+        if (step > 0) {
+            System.out.print("_".repeat(STEP_SIZE));
+            System.out.println();
+        }
+
+        int read;
+        while ((read = in.read(buffer, 0, DEFAULT_BUFFER_SIZE)) >= 0) {
+            out.write(buffer, 0, read);
+            transferred += read;
+            if (step > 0 && transferred > step) {
+                System.out.print('+');
+                transferred %= step;
+            }
+        }
+        System.out.println();
+    }
+}
--- src/Param.java
+++ src/Param.java
@@ -15,4 +15,20 @@ public enum Param {
         this.defaultValue = defaultValue;
         this.isFlag = isFlag;
     }
+
+    public static Param getByLongForm(String longForm) {
+        for (Param param : values()) {
+            if (param.longForm.equalsIgnoreCase(longForm))
+                return param;
+        }
+        return null;
+    }
+
+    public static Param getByShortForm(String shortForm) {
+        for (Param param : values()) {
+            if (param.shortForm.equalsIgnoreCase(shortForm))
+                return param;
+        }
+        return null;
+    }
 }
